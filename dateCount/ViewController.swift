ba36868e484c5714209befc907a702b8983bf4d5
ziuge <xwcjwwd@ewhain.net>
2022-07-13 15:10:25 +0900

    DatePicker UI

--- dateCount/ViewController.swift
+++ dateCount/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var day1Img: UIImageView!
+    @IBOutlet weak var day2Img: UIImageView!
+    @IBOutlet weak var day3Img: UIImageView!
+    @IBOutlet weak var day4Img: UIImageView!
+    
+    @IBOutlet weak var day1D: UILabel!
+    @IBOutlet weak var day2D: UILabel!
+    @IBOutlet weak var day3D: UILabel!
+    @IBOutlet weak var day4D: UILabel!
+    
+    @IBOutlet weak var day1Day: UILabel!
+    @IBOutlet weak var day2Day: UILabel!
+    @IBOutlet weak var day3Day: UILabel!
+    @IBOutlet weak var day4Day: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
